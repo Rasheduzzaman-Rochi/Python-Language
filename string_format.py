0122610ba02183de9cdbf0b6d2e6a0d8697b23d1
rasheduzzamanrochi <rasheduzzaman2003@gmail.com>
2026-05-20 22:53:29 +0600

    Refactor string formatting and remove input prompts

--- string_format.py
+++ string_format.py
@@ -1,2 +1,8 @@
-n = input("Enter your name: ")
-print("Hello, {}. How are you?".format(n))
+# n = input("Enter your name: ")
+# print("Hello, {}. How are you?".format(n))
+
+age = 25
+fast_name = "John"
+last_name = "Doe"
+txt = "My name is {} {} and I am {} years old.".format(fast_name, last_name, age)
+print(txt)
